c030849a70545dea83b86f76beb738722638f90f
softcomet <professinalcertifiedsenior.dev@gmail.com>
2018-02-21 08:50:06 +0100

    final result

--- GraphComputation.py
+++ GraphComputation.py
@@ -32,9 +32,52 @@ z_2d = tf.matmul(tf.reshape(z, [n_values, 1]), tf.reshape(z, [1, n_values]))
 plt.imshow(z_2d.eval())
 plt.show()
 
-# For fun let's create a gabor patch:
+# For fun let's create a gabor patch
 x = tf.reshape(tf.sin(tf.linspace(-3.0, 3.0, n_values)), [n_values, 1])
 y = tf.reshape(tf.ones_like(x), [1, n_values])
 z = tf.multiply(tf.matmul(x, y), z_2d)
 plt.imshow(z.eval())
 plt.show()
+
+# list all the operations of a graph
+ops = tf.get_default_graph().get_operations()
+print([op.name for op in ops])
+
+# creating a generic function for computing the same thing
+
+def gabor(n_values=32, sigma=1.0, mean=0.0):
+    x = tf.linspace(-3.0, 3.0, n_values)
+    z = (tf.exp(tf.negative(tf.pow(x - mean, 2.0) /
+                       (2.0 * tf.pow(sigma, 2.0)))) *
+         (1.0 / (sigma * tf.sqrt(2.0 * 3.1415))))
+    gauss_kernel = tf.matmul(
+        tf.reshape(z, [n_values, 1]), tf.reshape(z, [1, n_values]))
+    x = tf.reshape(tf.sin(tf.linspace(-3.0, 3.0, n_values)), [n_values, 1])
+    y = tf.reshape(tf.ones_like(x), [1, n_values])
+    gabor_kernel = tf.multiply(tf.matmul(x, y), gauss_kernel)
+    return gabor_kernel
+
+plt.imshow(gabor().eval())
+plt.show()
+
+# Implemented of function which can convolve
+def convolve(img, W):
+    # The W matrix is only 2D   
+    if len(W.get_shape()) == 2:
+        dims = W.get_shape().as_list() + [1, 1]
+        W = tf.reshape(W, dims)
+
+    if len(img.get_shape()) == 2:
+        # num x height x width x channels
+        dims = [1] + img.get_shape().as_list() + [1]
+        img = tf.reshape(img, dims)
+    elif len(img.get_shape()) == 3:
+        dims = [1] + img.get_shape().as_list()
+        img = tf.reshape(img, dims)       
+        W = tf.concat(2, [W, W, W])
+
+    # Stride is how many values to skip for the dimensions of
+    # num, height, width, channels
+    convolved = tf.nn.conv2d(img, W,
+                             strides=[1, 1, 1, 1], padding='SAME')
+    return convolved
